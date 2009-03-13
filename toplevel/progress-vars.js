59e0d4afb13999f115595af42be1389f51833ec8
Georgios Gousios <gousiosg@gmail.com>
2009-03-13 16:01:11 +0000

    Changes in page text

--- toplevel/progress-vars.js
+++ toplevel/progress-vars.js
@@ -1,1 +1,7 @@
+var last_prc=80;
+var last_numwords=80;
+var numwords_vals=new Array(12, 33, 43);
+var numwords_max=45;
+var chpr_targets="200,300,400,300,200,150";
+var chpr_actual="100,200,300,200,100,50";
 
